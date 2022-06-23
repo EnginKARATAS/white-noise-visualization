0e654c3f10e967e3c207256e55a75107c2614e4e
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-06-23 14:09:19 +0300

    Initalize canvas and resulation.

---
+++ index.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+var width = canvas.width;
+var height = canvas.height;
+var dx = 5,
+  dy = 5;
+
+var x = width/2,
+  y = height/2;
+
+
