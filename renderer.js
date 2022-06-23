d529a5fdaffd63076629501349b1c80c7ac3bae9
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-06-23 17:32:48 +0300

    Update render.js to better result

--- renderer.js
+++ renderer.js
@@ -7,12 +7,12 @@ var height = canvas.height; //500
 var initalX = 0,
   initalY = 0;
 
-var squareSize = 100
+var squareSize = 3
 let whiteNoise = () => {
   ctx.beginPath();
 
-  for (let x = 0; x < 800; x += 5) {
-    for (let y = 0; y < 450; y += 5) {
+  for (let x = 0; x < 800; x += 2) {
+    for (let y = 0; y < 450; y += 2) {
       ctx.moveTo(initalX + x, initalY + y);
       ctx.fillStyle = Math.round(Math.random())?"black":"white";
       ctx.fillRect(x, y, squareSize, squareSize);
